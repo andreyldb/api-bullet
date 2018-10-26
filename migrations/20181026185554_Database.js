fb32ebe0d7ea10acedacfe9352267b06a2f464c3
Leonardo MINORA <leonardo.minora@gmail.com>
2018-10-26 19:27:41 -0300

    versão com 1 rota para pegar as tarefas

--- migrations/20181026185554_Database.js
+++ migrations/20181026185554_Database.js
@@ -1,8 +1,22 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("tasks", function(table) {
+    // chave primária
+    table.increments("oid");
+
+    // estrutura
+    table.string("title", 50).notNullable();
+    table.string("description", 250).notNullable();
+    table.boolean("deleted");
+    table.boolean("done");
+
+    // timestamp
+    table
+      .timestamp("created_at")
+      .notNullable()
+      .defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("tasks");
 };
