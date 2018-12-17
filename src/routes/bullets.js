fe4b85a5213e7ba6d02d7e1bd17d69372aaf69e9
Arthur Craveiro <arthur.craveiro@gmail.com>
2018-12-17 14:52:46 -0300

    api de andrey e alessandra

--- src/routes/bullets.js
+++ src/routes/bullets.js
@@ -2,8 +2,8 @@ import knex from "../config/knex";
 
 const requestHandler = (request, reply) => {
   return knex
-    .from("bullets")
-    .where({ deleted: false })
+    .from("bullet")
+    // .where({ deleted: false })
     .select("id", "title", "description", "done", "date")
     .then(results => reply.response(results))
     .catch(err => console.log(err));
--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import Hapi from "hapi";
 
-import { root, tasks, bullets } from "./routes";
+import { root, bullets } from "./routes";
 
 const server = new Hapi.Server({
   port: process.env.PORT || 8000
 });
 
 const init = async () => {
-  server.route([].concat(root).concat(tasks).concat(bullets));
+  server.route([].concat(root).concat(bullets));
 
   await server.start();
   console.log("Server is running");
