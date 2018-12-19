dc0bfd974d38914f5d6db5f884e55c2eec2c2dd2
Gabriel Augusto <gabriel_abcunha@hotmail.com>
2018-12-19 14:33:02 -0300

    minorinha, estamos nos esforçando

--- seeds/Bullets.js
+++ seeds/Bullets.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('bullet').insert([
-        {id: 1, title: 'teste', description: 'testeteste', done: true, date: '2018-12-17'},
-        {id: 2, title: 'teste', description: 'testeteste', done: true, date: '2018-12-18'},
-        {id: 3, title: 'teste', description: 'testeteste', done: false, date: '2018-12-19'}
+        {id: 1, title: 'Passear com Tobby', description: 'Andar com meu cachorrinho por volta das 16h hoje', done: true, date: '2018-12-17'},
+        {id: 2, title: 'Passar de ano em Minora', description: 'Com muito esforço, vanglória e perseverança, eu e Alessandra encerramos este ano aprovados', done: true, date: '2018-12-18'},
+        {id: 3, title: 'Feliz Natal e boas festas', description: 'Dos seus alunos favoritos, Alessandra e Andrey', done: false, date: '2018-12-19'}
       ]);
     });
 };
--- src/routes/bullets.js
+++ src/routes/bullets.js
@@ -23,7 +23,153 @@ const bullets = [
     handler: (request, reply) => {
       return requestHandler(request, reply);
     }
+  },
+  {
+    method: "POST",
+    path: "/bullets",
+    handler: (request, reply) => {
+      try {
+        let { title, description } = JSON.parse(request.payload);
+        if (title === undefined) {
+          title = "";
+          return reply.response({ error: "undefined title" }).code(400);
+        }
+        if (description === undefined) {
+          description = "";
+        }
+        const bullet = {
+          title: title,
+          description: description,
+          done: false,
+          date: '2018-12-19',
+        };
+        return knex
+          .into("bullet")
+          .insert(bullet)
+          .returning("id")
+          .then(result => {
+            bullet.id = result[0];
+            return reply.response({ status: "inserted", data: bullet }).code(201);
+          })
+          .catch(err => {
+            return reply.response(err).code(400);
+          });
+      } catch (err) {
+        return reply
+          .response({ error: "undefined bullet in json object" })
+          .code(400);
+      }
+    }
+  },
+  {
+    method: "PUT",
+    path: "/bullets/{bullet_id}",
+    handler: (request, reply) => {
+      try {
+        const { title, description } = JSON.parse(request.payload);
+        const id = request.params.bullet_id;
+        let bullet = {};
+        if (title != undefined) {
+          bullet.title = title;
+        }
+        if (description != undefined) {
+          bullet.description = description;
+        }
+        return knex("bullet")
+          .where("id", id)
+          .update(bullet)
+          .then(result =>
+            knex("bullet")
+              .where("id", id)
+              .select("id", "title", "description", "done", "date")
+              .then(result =>
+                reply.response({ status: "updated", data: result[0] }).code(200)
+              )
+          )
+          .catch(err => reply.response(err).code(401));
+      } catch (err) {
+        return reply.response(err).code(401);
+      }
+    }
+  },
+  {
+    method: "PUT",
+    path: "/bullets/{bullet_id}/done",
+    handler: (request, reply) => {
+      try {
+        const id = request.params.bullet_id;
+        let bullet = { done: true };
+        return knex("bullet")
+          .where("id", id)
+          .update(bullet)
+          .then(result =>
+            knex("bullet")
+              .where("id", id)
+              .select("id", "title", "description", "done", "date")
+              .then(result =>
+                reply.response({ status: "done", data: result[0] }).code(200)
+              )
+          )
+          .catch(err => reply.response(err).code(401));
+      } catch (err) {
+        return reply.response(err).code(401);
+      }
+    }
+  },
+  {
+    method: "PUT",
+    path: "/bullets/{bullet_id}/undone",
+    handler: (request, reply) => {
+      try {
+        const id = request.params.bullet_id;
+        let bullet = { done: false };
+        return knex("bullet")
+          .where("id", id)
+          .update(bullet)
+          .then(result =>
+            knex("bullet")
+              .where("id", id)
+              .select("id", "title", "description", "done", "date")
+              .then(result =>
+                reply.response({ status: "undone", data: result[0] }).code(200)
+              )
+          )
+          .catch(err => reply.response(err).code(401));
+      } catch (err) {
+        return reply.response(err).code(401);
+      }
+    }
+  },
+  {
+    method: "DELETE",
+    path: "/bullets/{bullet_id}",
+    handler: (request, reply) => {
+      const id = request.params.bullet_id;
+      return knex("bullet")
+        .where("id", id)
+        .update({ deleted: true })
+        .then(result => {
+          console.log(result);
+          if (result === 0) {
+            return reply
+              .response({
+                status: "not deleted",
+                message: "bullet not found!"
+              })
+              .code(409);
+          } else {
+            return knex("bullet")
+              .where("id", id)
+              .select("id", "title", "description", "done", "date")
+              .then(result =>
+                reply.response({ status: "deleted", data: result[0] }).code(200)
+              );
+          }
+        })
+        .catch(err => reply.response(err).code(401));
+    }
   }
+
 ];
 
 export default bullets;
